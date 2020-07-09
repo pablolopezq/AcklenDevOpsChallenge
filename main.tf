f07be24b7fa0c89e35c836eb293f320d62ed18ec
Pablo <pablojlopez@unitec.edu>
2020-07-09 17:51:33 -0600

    Infrastructure done - ToDo: Organize tf code

--- main.tf
+++ main.tf
@@ -1,41 +1,116 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+data "aws_vpc" "default" {
+  id = "vpc-2193494a"
 }
 
-resource "aws_internet_gateway" "ig" {
-  vpc_id = aws_vpc.main.id
+data "aws_internet_gateway" "gateway" {
+  internet_gateway_id = "igw-936b2ffb"
 }
 
 resource "aws_subnet" "subnet_one" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.0.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id            = data.aws_vpc.default.id
+  cidr_block        = "172.31.150.0/24"
+  availability_zone = "us-east-2a"
 }
 
 resource "aws_subnet" "subnet_two" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1b"
+  vpc_id            = data.aws_vpc.default.id
+  cidr_block        = "172.31.100.0/24"
+  availability_zone = "us-east-2b"
+}
+
+resource "aws_eip" "eip_one" {
+  instance                  = aws_instance.instance_one.id
+  vpc                       = true
+  associate_with_private_ip = aws_instance.instance_one.private_ip
+}
+
+resource "aws_eip" "eip_two" {
+  instance                  = aws_instance.instance_two.id
+  vpc                       = true
+  associate_with_private_ip = aws_instance.instance_two.private_ip
 }
 
 resource "aws_instance" "instance_one" {
-  ami           = "ami-0fcfd45b96222a2ae"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.subnet_one.id
+  ami             = "ami-0e4c8f89e102079c1"
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.subnet_one.id
+  security_groups = [aws_security_group.sec_group.id]
 }
 
 resource "aws_instance" "instance_two" {
-  ami           = "ami-0fcfd45b96222a2ae"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.subnet_two.id
+  ami             = "ami-0e4c8f89e102079c1"
+  instance_type   = "t2.micro"
+  subnet_id       = aws_subnet.subnet_two.id
+  security_groups = [aws_security_group.sec_group.id]
 }
 
+
 resource "aws_lb" "app_load_balancer" {
-  subnets  = [aws_subnet.subnet_one.id, aws_subnet.subnet_two.id]
-  internal = false
-  #depends_on = [aws_internet_gateway.ig]
+  subnets            = [aws_subnet.subnet_one.id, aws_subnet.subnet_two.id]
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sec_group.id]
+  ip_address_type    = "ipv4"
+}
+
+resource "aws_security_group" "sec_group" {
+  vpc_id = data.aws_vpc.default.id
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb_target_group" "target_group_one" {
+  vpc_id   = data.aws_vpc.default.id
+  port     = 80
+  protocol = "HTTP"
+}
+
+resource "aws_lb_target_group" "target_group_two" {
+  vpc_id   = data.aws_vpc.default.id
+  port     = 80
+  protocol = "HTTP"
+}
+
+resource "aws_lb_target_group_attachment" "target_one" {
+  target_group_arn = aws_lb_target_group.target_group_one.arn
+  target_id        = aws_instance.instance_one.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "target_two" {
+  target_group_arn = aws_lb_target_group.target_group_two.arn
+  target_id        = aws_instance.instance_two.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "listener_one" {
+  load_balancer_arn = aws_lb.app_load_balancer.arn
+  port              = 80
+  default_action {
+    type = "forward"
+
+    forward {
+      target_group {
+        arn = aws_lb_target_group.target_group_one.arn
+      }
+      target_group {
+        arn = aws_lb_target_group.target_group_two.arn
+
+      }
+    }
+  }
 }
